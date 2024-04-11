e4d3042c0859ab4b9f492c52960f0d0c71daa977
Nico Alvarez <nicolas.alv3@gmail.com>
2024-04-11 13:33:41 -0300

    Clean imports

--- src/module/product/adapter/controller/dto/update-product.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ProductRequestDto } from './REST-request/product-request.dto';
-
-export class UpdateProductDto extends PartialType(ProductRequestDto) {}
--- src/module/product/application/usecase/create-product-use-case.service.ts
+++ src/module/product/application/usecase/create-product-use-case.service.ts
@@ -1,9 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ProductRequestDto } from '../../adapter/controller/dto/REST-request/product-request.dto';
-import { UpdateProductDto } from '../../adapter/controller/dto/update-product.dto';
-import { InjectModel } from '@nestjs/mongoose';
 import { Product } from '../../domain/product.entity';
-import { Model } from 'mongoose';
 import { CreateProductCommand } from '../port/in/CreateProductCommand';
 import { ProductRepository } from '../port/out/ProductRepository';
 
